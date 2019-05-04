80887922abee855b893a370afb5ade0415b393d3
Carlos Castillo <cmaxhm@yahoo.com>
2019-05-04 17:48:46 -0500

    - Percentage scroll indicator - Minor changes

--- angular/children-routes/src/app/app-routing.module.ts
+++ angular/children-routes/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {ChildRouteComponent} from './components/child-route/child-route.component';
-import {ChildRouteChildComponent} from './components/child-route/child-route-child/child-route-child.component';
+import { ChildRouteComponent } from './components/child-route/child-route.component';
+import { ChildRouteChildComponent } from './components/child-route/child-route-child/child-route-child.component';
 
 const routes: Routes = [
-  { path: 'child',
+  {
+    path: 'child',
     component: ChildRouteComponent,
     children: [
       { path: 'child', component: ChildRouteChildComponent }
@@ -16,4 +17,5 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
