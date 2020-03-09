6afbacba3554e1b618c351a51f67e631d6e77b7f
Carlos Castillo <cmaxhm@yahoo.com>
2020-03-08 22:36:04 -0500

    Images preview

---
+++ js/images-preview/images-preview.js
@@ -0,0 +1,26 @@
+const form = document.getElementById('form');
+const fileInput = document.getElementById('input');
+const imagesContainer = document.getElementById('images');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  
+  imagesContainer.innerHTML = '';
+  
+  let fileList = [...fileInput.files];
+  
+  fileList.forEach((file) => {
+    const fileReader = new FileReader();
+  
+    fileReader.readAsDataURL(file);
+    
+    fileReader.onload = () => {
+      const imageElement = document.createElement('img');
+      
+      imageElement.width = 200;
+      imageElement.src = fileReader.result;
+  
+      imagesContainer.append(imageElement);
+    };
+  });
+});
